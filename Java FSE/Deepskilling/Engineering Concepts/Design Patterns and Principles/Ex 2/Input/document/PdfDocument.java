59e79299a5d64fe3e7cebdc167e2302b99a272be
vKarthiga <karthigavenkat971@gmail.com>
2025-06-17 22:19:36 +0530

    Create PdfDocument.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Patterns and Principles/Ex 2/Input/document/PdfDocument.java
@@ -0,0 +1,8 @@
+package com.factory.document;
+
+public class PdfDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening PDF document...");
+    }
+}
