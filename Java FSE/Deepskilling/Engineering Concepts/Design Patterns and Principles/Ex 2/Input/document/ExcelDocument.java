a40a8d80e87bdad3b3c9bd46e03e29310d90ac68
vKarthiga <karthigavenkat971@gmail.com>
2025-06-17 22:17:25 +0530

    Create ExcelDocument.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Patterns and Principles/Ex 2/Input/document/ExcelDocument.java
@@ -0,0 +1,8 @@
+package com.factory.document;
+
+public class ExcelDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening Excel document...");
+    }
+}
