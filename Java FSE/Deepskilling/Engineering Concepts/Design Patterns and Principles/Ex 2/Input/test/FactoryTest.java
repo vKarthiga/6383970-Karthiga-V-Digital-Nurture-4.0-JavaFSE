29c1290563603275e8502d7880391c599431cf72
vKarthiga <karthigavenkat971@gmail.com>
2025-06-17 22:28:41 +0530

    Create FactoryTest.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Patterns and Principles/Ex 2/Input/test/FactoryTest.java
@@ -0,0 +1,21 @@
+package com.factory.test;
+
+import com.factory.document.Document;
+import com.factory.factory.*;
+
+public class FactoryTest {
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
