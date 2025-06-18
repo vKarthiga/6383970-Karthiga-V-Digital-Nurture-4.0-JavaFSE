57ccfd08efc904d85fd4052e425e1d16780cf599
vKarthiga <karthigavenkat971@gmail.com>
2025-06-18 19:22:43 +0530

    Create Employee.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Algorithms_Data Structures/Ex 4/Input/Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String position;
+    private double salary;
+
+    public Employee(int employeeId, String name, String position, double salary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return employeeId + " | " + name + " | " + position + " | $" + salary;
+    }
+}
