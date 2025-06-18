26a60a58c88f48f759f4bbe61f76bdae3ada49d1
vKarthiga <karthigavenkat971@gmail.com>
2025-06-18 10:52:00 +0530

    Create StudentController.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Patterns and Principles/Ex 10/Input/mvc/StudentController.java
@@ -0,0 +1,39 @@
+package mvc;
+
+public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setStudentName(String name) {
+        model.setName(name);
+    }
+
+    public String getStudentName() {
+        return model.getName();
+    }
+
+    public void setStudentGrade(String grade) {
+        model.setGrade(grade);
+    }
+
+    public String getStudentGrade() {
+        return model.getGrade();
+    }
+
+    public void setStudentId(String id) {
+        model.setId(id);
+    }
+
+    public String getStudentId() {
+        return model.getId();
+    }
+
+    public void updateView() {
+        view.printStudentDetails(model.getId(), model.getName(), model.getGrade());
+    }
+}
