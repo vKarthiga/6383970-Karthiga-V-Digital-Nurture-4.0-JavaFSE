edefdf1480fef4fbf129e40af27c8d910c212e9b
vKarthiga <karthigavenkat971@gmail.com>
2025-06-18 19:17:02 +0530

    Create SearchService.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Algorithms_Data Structures/Ex 2/Input/SearchService.java
@@ -0,0 +1,23 @@
+public class SearchService {
+
+    public static Product linearSearch(Product[] products, String name) {
+        for (Product product : products) {
+            if (product.getProductName().equalsIgnoreCase(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static Product binarySearch(Product[] products, String name) {
+        int left = 0, right = products.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int cmp = products[mid].getProductName().compareToIgnoreCase(name);
+            if (cmp == 0) return products[mid];
+            else if (cmp < 0) left = mid + 1;
+            else right = mid - 1;
+        }
+        return null;
+    }
+}
