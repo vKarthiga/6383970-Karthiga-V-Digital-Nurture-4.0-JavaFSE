a48b3b61a6211b4565599b34def5d3daa0ce1bec
vKarthiga <karthigavenkat971@gmail.com>
2025-06-18 10:36:30 +0530

    Create CreditCardPayment.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Patterns and Principles/Ex 8/Input/strategy/CreditCardPayment.java
@@ -0,0 +1,14 @@
+package strategy;
+
+public class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+
+    public CreditCardPayment(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " using Credit Card ending with " + cardNumber.substring(cardNumber.length() - 4));
+    }
+}
