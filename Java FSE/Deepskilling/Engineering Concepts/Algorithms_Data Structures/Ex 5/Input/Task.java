698893b22957f222b44bc3c07440b4b6022861d4
vKarthiga <karthigavenkat971@gmail.com>
2025-06-18 19:26:11 +0530

    Create Task.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Algorithms_Data Structures/Ex 5/Input/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+    int taskId;
+    String taskName;
+    String status;
+
+    public Task(int taskId, String taskName, String status) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return taskId + " | " + taskName + " | " + status;
+    }
+}
