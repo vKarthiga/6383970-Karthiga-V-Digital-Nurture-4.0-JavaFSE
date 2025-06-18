938690e357c58e2476b065c28ff500ad2f302d9f
vKarthiga <karthigavenkat971@gmail.com>
2025-06-18 10:52:49 +0530

    Create StudentView.java

---
+++ Java FSE/Deepskilling/Engineering Concepts/Design Patterns and Principles/Ex 10/Input/mvc/StudentView.java
@@ -0,0 +1,10 @@
+package mvc;
+
+public class StudentView {
+    public void printStudentDetails(String studentId, String studentName, String studentGrade) {
+        System.out.println("Student Details:");
+        System.out.println("ID    : " + studentId);
+        System.out.println("Name  : " + studentName);
+        System.out.println("Grade : " + studentGrade);
+    }
+}
