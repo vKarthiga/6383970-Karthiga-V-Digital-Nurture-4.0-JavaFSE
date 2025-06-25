49ba1238413c8a64574b87312c21c72e56111085
vKarthiga <karthigavenkat971@gmail.com>
2025-06-25 11:24:45 +0530

    Create AssertionsTest.java

---
+++ Java FSE/Deepskilling/JUnit_Mockito and SL4J/JUnit Testing Exercises/Ex 3/Input/AssertionsTest.java
@@ -0,0 +1,16 @@
+package com.example.junit_assertions_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+        assertEquals(5, 2 + 3);          
+        assertTrue(5 > 3);               
+        assertFalse(5 < 3);              
+        assertNull(null);               
+        assertNotNull(new Object());     
+    }
+}
