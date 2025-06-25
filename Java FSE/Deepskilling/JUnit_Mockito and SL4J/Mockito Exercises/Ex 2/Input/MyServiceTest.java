3a21c509136978ce2c8ea445c18ad132f9eb14a3
vKarthiga <karthigavenkat971@gmail.com>
2025-06-25 12:17:54 +0530

    Create MyServiceTest.java

---
+++ Java FSE/Deepskilling/JUnit_Mockito and SL4J/Mockito Exercises/Ex 2/Input/MyServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.mockito_verification;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}
