dc028b7e1527c018baecda24deaf773003191b9e
vKarthiga <karthigavenkat971@gmail.com>
2025-06-27 23:15:38 +0530

    Create Service.java

---
+++ Java FSE/Deepskilling/JUnit_Mockito and SL4J/Advanced Mockito Excercises/Ex 1/Input/Service.java
@@ -0,0 +1,15 @@
+package com.example.mock_demo;
+
+public class Service {
+	private Repository repository;
+
+    public Service(Repository repository) {
+        this.repository = repository;
+    }
+
+    public String processData() {
+        String data = repository.getData();
+        return "Processed " + data;
+    }
+
+}
