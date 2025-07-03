9b34016e876521c7e695ee25f52a0b5d0def7efa
vKarthiga <karthigavenkat971@gmail.com>
2025-07-03 10:13:03 +0530

    Create LibraryManagementApplication.java

---
+++ Java FSE/Deepskilling/Spring Core and Maven/Ex 2/Input/LibraryManagementApplication.java
@@ -0,0 +1,17 @@
+package com.example.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.example.library.service.BookService;
+
+public class LibraryManagementApplication {
+
+    private static ApplicationContext context;
+
+	public static void main(String[] args) {
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService bookService = (BookService) context.getBean("bookService");
+        bookService.listBooks(); // Should print the list of books
+    }
+}
