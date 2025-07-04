784d04e14ccff59884306491093b80cb9f3e4377
vKarthiga <karthigavenkat971@gmail.com>
2025-07-04 21:56:44 +0530

    Create Stock.java

---
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/2.spring data jpa/Ex 2/Input/Stock.java
@@ -0,0 +1,26 @@
+@Entity
+@Table(name = "stock")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "st_id")
+    private int id;
+
+    @Column(name = "st_code")
+    private String code;
+
+    @Column(name = "st_date")
+    private LocalDate date;
+
+    @Column(name = "st_open")
+    private BigDecimal open;
+
+    @Column(name = "st_close")
+    private BigDecimal close;
+
+    @Column(name = "st_volume")
+    private Long volume;
+
+    // Getters and Setters
+}
