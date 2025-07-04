6e6d8afd837a3eff5323fb494b123cc4333b5528
vKarthiga <karthigavenkat971@gmail.com>
2025-07-04 19:53:23 +0530

    Create CountryService.java

---
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/1.spring data jpa/Ex 1/Input/CountryService.java
@@ -0,0 +1,12 @@
+com.cognizant.ormlearn.service
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
