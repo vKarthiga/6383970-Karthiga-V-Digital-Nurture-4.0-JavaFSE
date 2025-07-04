ce7adb37ddb1f5b92da9f774798c2cdfdddce15a
vKarthiga <karthigavenkat971@gmail.com>
2025-07-04 20:03:41 +0530

    Create EmployeeService.java

---
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/1.spring data jpa/Ex 4/Input/EmployeeService.java
@@ -0,0 +1,11 @@
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public void addEmployee(Employee employee) {
+        employeeRepository.save(employee); // All handled under the hood!
+    }
+}
