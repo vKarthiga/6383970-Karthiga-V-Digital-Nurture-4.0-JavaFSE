c0f5c6d8fae65369933d160ea48709b56424a6b7
vKarthiga <karthigavenkat971@gmail.com>
2025-07-04 19:52:18 +0530

    Create Country.java

---
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/1.spring data jpa/Ex 1/Input/Country.java
@@ -0,0 +1,14 @@
+com.cognizant.ormlearn.model
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @Column(name = "co_code")
+    private String code;
+
+    @Column(name = "co_name")
+    private String name;
+
+    // getters, setters, toString
+}
