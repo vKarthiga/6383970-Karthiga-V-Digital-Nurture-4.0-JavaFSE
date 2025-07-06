2ae5aa19c84ea45aa3f7d7f65a902761e13dca52
vKarthiga <karthigavenkat971@gmail.com>
2025-07-06 23:02:08 +0530

    Update Question.java

--- Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/Question.java
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/Question.java
@@ -1,10 +1,13 @@
 @Entity
+@Table(name = "question")
 public class Question {
     @Id
+    @Column(name = "qt_id")
     private int id;
-    private String text;
-    private double score;
 
-    @OneToMany(mappedBy = "question")
+    @Column(name = "qt_text")
+    private String text;
+
+    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY)
     private List<Option> options;
 }
