73282c95958b4661132d3341ce8e50b9cd61bc73
vKarthiga <karthigavenkat971@gmail.com>
2025-07-06 23:01:10 +0530

    Update User.java

--- Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/User.java
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/User.java
@@ -1,9 +1,16 @@
 @Entity
+@Table(name = "user")
 public class User {
     @Id
+    @Column(name = "us_id")
     private int id;
-    private String username;
 
-    @OneToMany(mappedBy = "user")
+    @Column(name = "us_name")
+    private String name;
+
+    @Column(name = "us_email")
+    private String email;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Attempt> attempts;
 }
