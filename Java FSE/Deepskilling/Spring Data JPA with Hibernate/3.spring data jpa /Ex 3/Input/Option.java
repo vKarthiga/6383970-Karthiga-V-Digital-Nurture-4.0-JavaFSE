410165861f3157279b7aadc61aa5891f5c1b2bb8
vKarthiga <karthigavenkat971@gmail.com>
2025-07-06 23:02:25 +0530

    Update Option.java

--- Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/Option.java
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/Option.java
@@ -1,11 +1,20 @@
 @Entity
+@Table(name = "options")
 public class Option {
     @Id
+    @Column(name = "op_id")
     private int id;
+
+    @Column(name = "op_text")
     private String text;
-    private boolean isCorrect;
+
+    @Column(name = "op_score")
+    private double score;
 
     @ManyToOne
-    @JoinColumn(name = "question_id")
+    @JoinColumn(name = "op_qt_id")
     private Question question;
+
+    @OneToMany(mappedBy = "option", fetch = FetchType.LAZY)
+    private List<AttemptOption> attemptOptions;
 }
