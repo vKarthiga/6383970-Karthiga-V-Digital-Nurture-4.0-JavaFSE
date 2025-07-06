dc5a4f31e30b19c53995418cb8b6372fdfdf9bd5
vKarthiga <karthigavenkat971@gmail.com>
2025-07-06 22:58:30 +0530

    Create AttemptOption.java

---
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/AttemptOption.java
@@ -0,0 +1,15 @@
+@Entity
+public class AttemptOption {
+    @Id
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "attempt_question_id")
+    private AttemptQuestion attemptQuestion;
+
+    @ManyToOne
+    @JoinColumn(name = "option_id")
+    private Option option;
+
+    private boolean selected;
+}
