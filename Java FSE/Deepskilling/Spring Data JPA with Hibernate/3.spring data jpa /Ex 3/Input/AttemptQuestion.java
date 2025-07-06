1adb35d1b8b4783085c1400f2624ef50e7221de8
vKarthiga <karthigavenkat971@gmail.com>
2025-07-06 23:02:42 +0530

    Update AttemptQuestion.java

--- Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/AttemptQuestion.java
+++ Java FSE/Deepskilling/Spring Data JPA with Hibernate/3.spring data jpa /Ex 3/Input/AttemptQuestion.java
@@ -1,16 +1,18 @@
 @Entity
+@Table(name = "attempt_question")
 public class AttemptQuestion {
     @Id
+    @Column(name = "aq_id")
     private int id;
 
     @ManyToOne
-    @JoinColumn(name = "attempt_id")
+    @JoinColumn(name = "aq_at_id")
     private Attempt attempt;
 
     @ManyToOne
-    @JoinColumn(name = "question_id")
+    @JoinColumn(name = "aq_qt_id")
     private Question question;
 
-    @OneToMany(mappedBy = "attemptQuestion")
+    @OneToMany(mappedBy = "attemptQuestion", fetch = FetchType.LAZY)
     private List<AttemptOption> attemptOptions;
 }
