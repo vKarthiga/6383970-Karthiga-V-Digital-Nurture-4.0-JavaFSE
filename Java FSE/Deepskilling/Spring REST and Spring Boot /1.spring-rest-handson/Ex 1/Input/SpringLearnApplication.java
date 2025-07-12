dd24630129dce22f509d97744fa57ba6c4afa1c6
vKarthiga <karthigavenkat971@gmail.com>
2025-07-12 12:33:04 +0530

    Create SpringLearnApplication.java

---
+++ Java FSE/Deepskilling/Spring REST and Spring Boot /1.spring-rest-handson/Ex 1/Input/SpringLearnApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class SpringLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+    }
+}
