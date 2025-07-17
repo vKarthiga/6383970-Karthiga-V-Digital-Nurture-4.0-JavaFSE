21524701a837d6d288a79da5c0bf2c270d10fe1a
vKarthiga <karthigavenkat971@gmail.com>
2025-07-17 12:07:38 +0530

    Create EurekaDiscoveryServerApplication.java

---
+++ Java FSE/Deepskilling/Microservices/2.Microservices with API gateway/Ex 2/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+    }
+}
